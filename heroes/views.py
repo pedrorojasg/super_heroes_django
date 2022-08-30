89fee8da4441a93952776a644786fadf536ba9be
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-08-30 09:17:24 -0400

    Mejoras en estilos y rutas

--- heroes/views.py
+++ heroes/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import Template, Context, loader
 from heroes.models import SuperHeroe
+
+
+def inicio(request):
+    return render(request, "heroes/inicio.html")
 
 
 def listar_heroes(request):
     queryset = SuperHeroe.objects.all()
     diccionario = {'heroes': queryset}
-    plantilla = loader.get_template('heroes_list.html')
+    plantilla = loader.get_template('heroes/heroes_list.html')
     documento_html = plantilla.render(diccionario)
 
     return HttpResponse(documento_html)
--- super_heroes/urls.py
+++ super_heroes/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from heroes.views import inicio
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', inicio),
     path('heroes/', include('heroes.urls'))
 ]
